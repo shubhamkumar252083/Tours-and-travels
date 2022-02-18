8fb52e3abd2741750e143316c7faad375571984f
shubham kumar <shubhamkumar252083@gmail.com>
2022-02-19 00:13:02 +0530

    admin.py adding search and list display

--- destination/admin.py
+++ destination/admin.py
@@ -2,7 +2,27 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import *
+
+
+class DestinationAdmin(admin.ModelAdmin):
+    list_display = ("id", "country", "state", "city",
+                    "place_name", "price", "discount_price",)
+    search_fields = ("id", "state", "city", "place_name", "price",
+                     "discount_price", "slug", "continent", "country")
+
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "email", "phone")
+    search_fields = ("id", "name", "email", "phone", "address",
+                     "location", "guests", "arrivals", "leaving")
+
+
+class CustomerReviewAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "stars")
+    search_fields = ("id", "name", "stars", "review")
+
+
 admin.site.register(AboutUs)
-admin.site.register(Destination)
-admin.site.register(Booking)
-admin.site.register(CustomerReview)
+admin.site.register(Destination, DestinationAdmin)
+admin.site.register(Booking, BookingAdmin)
+admin.site.register(CustomerReview, CustomerReviewAdmin)
